2212087b708797d05314db0f82d31aa4d3b95e01
baldo-vela <baldo.vela+github@gmail.com>
2020-11-17 06:59:07 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
+class Book
+#fundamental properties of class
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+    
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+
+    def page_count=(num)
+        @page_count=num
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
+
+    #Behavioral methods
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
 
